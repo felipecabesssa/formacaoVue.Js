b235b281541a0a780d6672452c2baa38eb76a9ea
felipecabesssa <felipesoares100@hotmail.com>
2021-09-02 21:37:11 -0300

    Eventos customizados

--- alurapicV2/alurapic/src/components/home/Home.vue
+++ alurapicV2/alurapic/src/components/home/Home.vue
@@ -9,7 +9,7 @@
         <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
             <meu-painel :titulo="foto.titulo">
             <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-            <meu-botao tipo="button" rotulo="REMOVER" @click.native="remove(foto)"/>
+            <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)"/>
             </meu-painel>
         </li>
         </ul>
@@ -53,10 +53,7 @@ export default {
 
   methods: {
     remove(foto){
-
-      if(confirm('Confirma operação?')){
-        alert('Remover a foto!' + foto.titulo);
-      }
+      alert('Remover a foto!' + foto.titulo);      
     }
   },
 
