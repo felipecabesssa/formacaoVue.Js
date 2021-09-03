fd1c2ec46a2c177067b29555eb47e6cc4b9b8857
felipecabesssa <felipesoares100@hotmail.com>
2021-09-02 21:51:29 -0300

    Lapidando nosso componente

--- alurapicV2/alurapic/src/components/home/Home.vue
+++ alurapicV2/alurapic/src/components/home/Home.vue
@@ -9,7 +9,12 @@
         <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
             <meu-painel :titulo="foto.titulo">
             <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-            <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)"/>
+            <meu-botao 
+            tipo="button" 
+            rotulo="REMOVER" 
+            @botaoAtivado="remove(foto)"
+            :confirmacao="true"
+            estilo="padrao"/>
             </meu-painel>
         </li>
         </ul>
