76e9ec6fba2d39745227195349aa86dd2dca712d
felipecabesssa <felipesoares100@hotmail.com>
2021-09-02 21:56:45 -0300

    Validando propriedades

--- alurapicV2/alurapic/src/components/shared/botao/Botao.vue
+++ alurapicV2/alurapic/src/components/shared/botao/Botao.vue
@@ -4,7 +4,22 @@
 <script>
 export default {
 
-   props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+   props: {
+
+       tipo: {
+           required: true,
+           type: String
+       },
+
+       rotulo: {
+           required: true,
+           type: String
+       },
+
+       confirmacao: Boolean,
+       estilo: String
+
+   }
 
    methods: {
        disparaAcao() {
