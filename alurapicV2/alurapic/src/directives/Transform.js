d65bd35313bc5ec80441320c8d523edfea95b3fa
felipecabesssa <felipesoares100@hotmail.com>
2021-12-22 12:55:23 -0300

    Lidando com Formularios

--- alurapicV2/alurapic/src/directives/Transform.js
+++ alurapicV2/alurapic/src/directives/Transform.js
@@ -1,28 +1,35 @@
 import Vue from 'vue';
 
-Vue.directive('meu-transform',{
+Vue.directive('meu-transform', {
 
-    bind(el, binding, vnode){
+    bind(el, binding, vnode) {
 
         let current = 0;
 
-        el.addEventListener('dblclick', function(){
+        el.addEventListener('dblclick', function() {
 
-            let incremento = 90;
-            let animate = false;
+            let incremento = binding.value || 90;
+            let efeito;
 
-            if(binding.value){
+            if(!binding.arg || binding.arg == 'rotate') {
+                
+                if(binding.modifiers.reverse) {
+                    current-=incremento;
+                } else {
+                    current+=incremento;
+                }
 
-                incremento = binding.value.incremento;
-                animate = binding.value.animate;
-
+                efeito = `rotate(${current}deg)`;
+                
+            } else if(binding.arg == "scale") {
+                efeito = `scale(${incremento})`;
             }
 
-            current+=incremento;
-            el.style.transform = `rotate(${current}deg)`;
-            if(animate) el.style.transition = 'transform 0.5s';
-        });
+            el.style.transform = efeito;
 
+            if(binding.modifiers.animate) el.style.transition = 'transform 0.5s';
+        });
     }
 
 });
+
